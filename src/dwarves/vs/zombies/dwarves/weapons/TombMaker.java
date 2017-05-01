d1d1d101e925585fc3a84a33472410861f8ceab8
nessie2013 <thomasprentice01@hotmail.com>
2017-05-01 18:26:10 +0100

    Update TombMaker.java

--- src/dwarves/vs/zombies/dwarves/weapons/TombMaker.java
+++ src/dwarves/vs/zombies/dwarves/weapons/TombMaker.java
@@ -1,1 +1,121 @@
+package dwarves.vs.zombies.dwarves.weapons;
 
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
+import net.minecraft.server.v1_11_R1.NBTTagInt;
+import net.minecraft.server.v1_11_R1.NBTTagList;
+import net.minecraft.server.v1_11_R1.NBTTagString;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import dwarves.vs.zombies.Core;
+import dwarves.vs.zombies.Weapon;
+
+public class TombMaker extends Weapon {
+
+	Player player;
+	boolean usedSpecial = false;
+	int timer = 0;
+
+	public GreaterRuneblade(Player player)
+	{
+		super(true, true);
+		this.player = player;
+	}
+
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (short) 3);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.AQUA + "Tomb Maker");
+		ArrayList<String> lore = new ArrayList<String>();
+                lore.add(ChatColor.YELLOW + "Power: " ChatColor.AQUA + "4"
+		lore.add(ChatColor.YELLOW + "With this Shovel, you gain a Powerful Rampage for 5");
+		lore.add(ChatColor.YELLOW + "seconds when shoveling gravel. Additionak kills while");
+		lore.add(ChatColor.YELLOW + "on a rampage will allow you to extend the duration.");
+		lore.add(ChatColor.YELLOW + "Right Click to gain a haste buff to both dig and craft");
+		lore.add(ChatColor.YELLOW + "faster as well as put an additional resource item you");
+           	lore.add(ChatColor.YELLOW + "gather into the Shared Recource Chest");
+		meta.setLore(lore);
+		item.setItemMeta(meta);
+
+		net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+		NBTTagCompound compound = (nmsStack.hasTag()) ? nmsStack.getTag() : new NBTTagCompound();
+		NBTTagList modifiers = new NBTTagList();
+		NBTTagCompound damage = new NBTTagCompound();
+		damage.set("AttributeName", new NBTTagString("generic.attackDamage"));
+		damage.set("Name", new NBTTagString("generic.attackDamage"));
+		damage.set("Amount", new NBTTagInt(4));
+		damage.set("Operation", new NBTTagInt(0));
+		damage.set("UUIDLeast", new NBTTagInt(894654));
+		damage.set("UUIDMost", new NBTTagInt(2872));
+		modifiers.add(damage);
+
+		item = CraftItemStack.asBukkitCopy(nmsStack);
+
+		return item;
+	}
+
+	@Override
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	@Override
+	public void normal()
+	{
+
+	}
+
+	@Override
+	public void special()
+	{
+		if (usedSpecial)
+		{
+			player.sendMessage(ChatColor.DARK_AQUA + "You Cant Do That For " + timer
+					+ " Seconds.");
+			return;
+		}
+		usedSpecial = true;
+
+		player.playSound(player.getLocation(), "SOMETHING", 4F, 1F);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 200, 2, false, false), false);
+
+		timer = 15;
+		tombHasteTimer task = new tombHasteTimer();
+		task.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getInstance(), task, 0, 20));
+
+	}
+
+	private class tombHasteTimer implements Runnable {
+
+		private int id;
+
+		@Override
+		public void run()
+		{
+			if (timer == 0)
+			{
+				usedSpecial = false;
+				Bukkit.getScheduler().cancelTask(id);
+			}
+
+			timer -= 1;
+		}
+
+		public void setId(int id)
+		{
+			this.id = id;
+		}
+
+	}
+
+}
