3b8083193b8da5bfd2cac412f030e6c2e270a693
nessie2013 <thomasprentice01@hotmail.com>
2017-05-01 17:50:34 +0100

    Updated lore and effects

--- src/dwarves/vs/zombies/dwarves/weapons/AxeOfMalice.java
+++ src/dwarves/vs/zombies/dwarves/weapons/AxeOfMalice.java
@@ -35,26 +35,27 @@ public class AxeOfMalice extends Weapon {
 		ItemStack item = new ItemStack(Material.INK_SACK, 1, (short) 3);
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(ChatColor.AQUA + "Axe Of Malice");
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(Chatcolor.GOLD + "Power: " Chatcolor.Aqua + "20"
+		lore.add(ChatColor.GOLD + "With this Axe, you can Right Click to grant yourself a");
+		lore.add(ChatColor.GOLD + "Powerful Rampage for 8 seconds. Additional kills while");
+		lore.add(ChatColor.GOLD + "on a rampage will allow you to extend the duration.");
+		lore.add(ChatColor.GOLD + "This ability has a 60 second cooldown that is reduced");
+		lore.add(ChatColor.GOLD + "by 1 for every kill or 4 seconds for Bow kills.");
+		meta.setLore(lore);
 		item.setItemMeta(meta);
 
 		net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
 		NBTTagCompound compound = (nmsStack.hasTag()) ? nmsStack.getTag() : new NBTTagCompound();
-
 		NBTTagList modifiers = new NBTTagList();
-		{
-			NBTTagCompound damage = new NBTTagCompound();
-			damage.set("AttributeName", new NBTTagString("generic.attackDamage"));
-			damage.set("Name", new NBTTagString("generic.attackDamage"));
-			damage.set("Amount", new NBTTagInt(20));
-			damage.set("Operation", new NBTTagInt(0));
-			damage.set("UUIDLeast", new NBTTagInt(894654));
-			damage.set("UUIDMost", new NBTTagInt(2872));
-			damage.set("Slot", new NBTTagString("mainhand"));
-			modifiers.add(damage);
-		}
-
-		compound.set("AttributeModifiers", modifiers);
-
+		NBTTagCompound damage = new NBTTagCompound();
+		damage.set("AttributeName", new NBTTagString("generic.attackDamage"));
+		damage.set("Name", new NBTTagString("generic.attackDamage"));
+		damage.set("Amount", new NBTTagInt(20));
+		damage.set("Operation", new NBTTagInt(0));
+		damage.set("UUIDLeast", new NBTTagInt(894654));
+		damage.set("UUIDMost", new NBTTagInt(2872));
+		modifiers.add(damage);
 		nmsStack.setTag(compound);
 
 		item = CraftItemStack.asBukkitCopy(nmsStack);
@@ -87,8 +88,8 @@ public class AxeOfMalice extends Weapon {
 		usedSpecial = true;
 
 		player.playSound(player.getLocation(), "SOMETHING", 4F, 1F);
-		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 50, 3, false, false), false);
-		player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 50, 1, false);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 160, 2, false, false), false);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 160, 0, false);
 				false), false);
 
 		timer = 60;
