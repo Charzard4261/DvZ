ce8f877fbe2ebf711088c17112a47ea9bcf92d45
nessie2013 <thomasprentice01@hotmail.com>
2017-05-01 17:29:50 +0100

    Update ElvenDagger.java

--- src/dwarves/vs/zombies/dwarves/weapons/ElvenDagger.java
+++ src/dwarves/vs/zombies/dwarves/weapons/ElvenDagger.java
@@ -1,1 +1,121 @@
+package dwarves.vs.zombies.dwarves.weapons;
+
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
+import net.minecraft.server.v1_11_R1.NBTTagInt;
+import net.minecraft.server.v1_11_R1.NBTTagList;
+import net.minecraft.server.v1_11_R1.NBTTagString;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import dwarves.vs.zombies.Core;
+import dwarves.vs.zombies.Weapon;
+
+public class GreaterRuneblade extends Weapon {
+
+	Player player;
+	boolean usedSpecial = false;
+	int timer = 0;
+
+	public GreaterRuneblade(Player player)
+	{
+		super(true, true);
+		this.player = player;
+	}
+
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (short) 3);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.AQUA + "Elven Dagger");
+		ArrayList<String> lore = new ArrayList<String>();
+    lore.add(ChatColor.GOLD + "Power: " ChatColor.AQUA + "15"
+		lore.add(ChatColor.GOLD + "With this Dagger, you move more quickly and your");
+		lore.add(ChatColor.GOLD + "hits poison demons for 5 seconds. Right Click to");
+		lore.add(ChatColor.GOLD + "Eviserate which deals massive damage killing most");
+		lore.add(ChatColor.GOLD + "demons. This ability has a 60 second cooldown that");
+		lore.add(ChatColor.GOLD + "is reduced by 10 seconds for every bow kill.");
+		meta.setLore(lore);
+		item.setItemMeta(meta);
+
+		net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+		NBTTagCompound compound = (nmsStack.hasTag()) ? nmsStack.getTag() : new NBTTagCompound();
+		NBTTagList modifiers = new NBTTagList();
+		NBTTagCompound damage = new NBTTagCompound();
+		damage.set("AttributeName", new NBTTagString("generic.attackDamage"));
+		damage.set("Name", new NBTTagString("generic.attackDamage"));
+		damage.set("Amount", new NBTTagInt(15));
+		damage.set("Operation", new NBTTagInt(0));
+		damage.set("UUIDLeast", new NBTTagInt(894654));
+		damage.set("UUIDMost", new NBTTagInt(2872));
+		modifiers.add(damage);
+		nmsStack.setTag(compound);
+
+		item = CraftItemStack.asBukkitCopy(nmsStack);
+
+		return item;
+	}
+
+	@Override
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	@Override
+	public void normal()
+	{
+
+	}
+
+	@Override
+	public void special()
+	{
+		if (usedSpecial)
+		{
+			player.sendMessage(ChatColor.DARK_AQUA + "You Cant Do That For " + timer
+					+ " Seconds.");
+			return;
+		}
+		usedSpecial = true;
+
+		player.playSound(player.getLocation(), "SOMETHING", 4F, 1F);
+
+		timer = 60;
+		EviserateTimer task = new EviserateTimer();
+		task.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getInstance(), task, 0, 20));
+
+	}
+
+	private class EviserateTimer implements Runnable {
+
+		private int id;
+
+		@Override
+		public void run()
+		{
+			if (timer == 0)
+			{
+				usedSpecial = false;
+				Bukkit.getScheduler().cancelTask(id);
+			}
+
+			timer -= 1;
+		}
+
+		public void setId(int id)
+		{
+			this.id = id;
+		}
+
+	}
+
+}
 
