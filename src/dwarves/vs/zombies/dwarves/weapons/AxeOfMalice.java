c7ab063ed633fbb4f70ebe58d457e15c171e90dc
nessie2013 <thomasprentice01@hotmail.com>
2017-05-01 17:01:43 +0100

    Axe Of Malice

---
+++ src/dwarves/vs/zombies/dwarves/weapons/AxeOfMalice.java
@@ -0,0 +1,116 @@
+package dwarves.vs.zombies.dwarves.weapons;
+
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
+import net.minecraft.server.v1_11_R1.NBTTagInt;
+import net.minecraft.server.v1_11_R1.NBTTagList;
+import net.minecraft.server.v1_11_R1.NBTTagString;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import dwarves.vs.zombies.Core;
+import dwarves.vs.zombies.Weapon;
+
+public class AxeOfMalice extends Weapon {
+
+	Player player;
+	boolean usedSpecial = false;
+	int timer = 0;
+
+	public AxeOfMalice(Player player)
+	{
+		super(true, true);
+		this.player = player;
+	}
+
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (short) 2);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.RED + "Axe Of Malice");
+		item.setItemMeta(meta);
+
+		net.minecraft.server.v1_11_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+		NBTTagCompound compound = (nmsStack.hasTag()) ? nmsStack.getTag() : new NBTTagCompound();
+
+		NBTTagList modifiers = new NBTTagList();
+		{
+			NBTTagCompound damage = new NBTTagCompound();
+			damage.set("AttributeName", new NBTTagString("generic.attackDamage"));
+			damage.set("Name", new NBTTagString("generic.attackDamage"));
+			damage.set("Amount", new NBTTagInt(20));
+			damage.set("Operation", new NBTTagInt(0));
+			damage.set("UUIDLeast", new NBTTagInt(894654));
+			damage.set("UUIDMost", new NBTTagInt(2872));
+			damage.set("Slot", new NBTTagString("mainhand"));
+			modifiers.add(damage);
+    
+  }
+
+	@Override
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	@Override
+	public void normal()
+	{
+
+	}
+
+	@Override
+	public void special()
+	{
+		if (usedSpecial)
+		{
+			player.sendMessage(ChatColor.DARK_AQUA + "YOU MUST WAIT: " + timer
+					+ " SECONDS BEFORE YOU USE MY POWER.");
+			return;
+		}
+
+		usedSpecial = true;
+
+		player.playSound(player.getLocation(), "SOMETHING", 4F, 1F);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 50, 3, false, false), false);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 50, 1, false);
+				false), false);
+
+		timer = 60;
+    
+    axeofmaliceTimer task = new axeofmaliceTimer();
+		task.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getInstance(), task, 0, 20));
+
+	}
+
+	private class axeofmaliceTimer implements Runnable {
+
+		private int id;
+
+		@Override
+		public void run()
+		{
+			timer -= 1;
+
+			if (timer == 0)
+			{
+				usedSpecial = false;
+				Bukkit.getScheduler().cancelTask(id);
+			}
+		}
+
+		public void setId(int id)
+		{
+			this.id = id;
+		}
+
+	}
+
+}
