5178f3d7bb2f0c308c07516a54692a1104778059
nessie2013 <thomasprentice01@hotmail.com>
2017-05-05 11:45:30 +0100

    Update Crossbow.java

--- src/dwarves/vs/zombies/dwarves/bows/Crossbow.java
+++ src/dwarves/vs/zombies/dwarves/bows/Crossbow.java
@@ -1,1 +1,91 @@
+package dwarves.vs.zombies.dwarves.bows;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import dwarves.vs.zombies.Bow;
+import dwarves.vs.zombies.Core;
+
+public class DragonskinBow extends Bow implements Listener {
+
+	Player player;
+	public int timer;
+	public boolean usedSpecial;
+
+	@Override
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.BOW);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.AQUA + "Crossbow");
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.YELLOW + "Power: " + ChatColor.AQUA + "50");
+		lore.add(ChatColor.YELLOW + "With this Crossbow, your arrors fire at full power");
+		lore.add(ChatColor.YELLOW + "without requiring you to draw a bow first allowing");
+		lore.add(ChatColor.YELLOW + "You to move and shoot.");
+		meta.setLore(lore);
+		meta.setUnbreakable(true);
+		item.setItemMeta(meta);
+		return item;
+	}
+
+	@Override
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	@Override
+	public void special()
+	{
+		if (usedSpecial)
+		{
+			player.sendMessage(ChatColor.DARK_AQUA + "You must wait " + timer
+					+ " more seconds to do that.");
+			return;
+		}
+		usedSpecial = true;
+
+		Arrow arrow = (Arrow) event.getProjectile();
+		arrow.setFireTicks(0);
+		arrow.setVelocity(arrow.getVelocity().multiply(1));
+		arrow.setKnockbackStrength(1);
+		arrow.setCritical(false);
+		arrow.spigot().setDamage(50); // Damage
+
+		timer = 0.5;
+		EviserateTimer task = new EviserateTimer();
+		task.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getInstance(), task, 0, 20));
+
+	}
+
+	private class Timer implements Runnable {
+
+		private int id;
+
+		@Override
+		public void run()
+		{
+			timer -= 1;
+
+			if (timer == 0)
+			{
+				usedSpecial = false;
+				Bukkit.getScheduler().cancelTask(id);
+			}
+		}
+
+		public void setId(int id)
+		{
+			this.id = id;
+		}
+
+	}
+}
