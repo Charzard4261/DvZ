b37f80228944e6079a5cec47e9f3ea06a1b61ef5
Charzard4261 <CharzardX4261@gmail.com>
2017-05-10 20:44:18 +0100

    Update Monster.java

--- src/dwarves/vs/zombies/monsters/Monster.java
+++ src/dwarves/vs/zombies/monsters/Monster.java
@@ -1,12 +1,33 @@
 package dwarves.vs.zombies.monsters;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
-public class Monster {
-
+public abstract class Monster {
+	
+	private UUID uuid;
+	
+	public Monster (UUID uuid)
+	{
+		this.uuid = uuid;
+	}
+	
 	public Player getPlayer()
 	{
-		return null;
+		return Bukkit.getPlayer(uuid);
 	}
-
+	
+	public abstract void equipArmour();
+	
+	public abstract ArrayList<ItemStack> getItems();
+	
+	public abstract void onLeftClick(PlayerInteractEvent event);
+	
+	public abstract void onRightClick(PlayerInteractEvent event);
+	
 }
