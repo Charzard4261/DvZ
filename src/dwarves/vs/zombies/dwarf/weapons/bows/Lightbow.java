6c809e4a81f7e393b9cb510f366de4d6c7feeef3
nessie2013 <thomasprentice01@hotmail.com>
2018-03-02 12:51:59 +0000

    this should be the updated file of lightbow
    the spawn lamp has not been implemented yet

---
+++ src/dwarves/vs/zombies/dwarf/weapons/bows/Lightbow.java
@@ -0,0 +1,57 @@
+package dwarves.vs.zombies.dwarf.weapons.bows;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import dwarves.vs.zombies.dwarf.Dwarf;
+import dwarves.vs.zombies.dwarf.superclasses.DwarfBow;
+
+public class Lightbow extends DwarfBow {
+
+	public Lightbow(Dwarf dwarf)
+	{
+		super(dwarf, 30);
+	}
+
+	@Override
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.BOW);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.AQUA + "Lightbow");
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.YELLOW + "Power: " + ChatColor.AQUA + "50");
+		lore.add(ChatColor.YELLOW + "With this Bow, arrows that do not hit a monster will");
+		lore.add(ChatColor.YELLOW + "spawn an Enchanted Lamp that lasts for 5 seconds.");
+		lore.add(ChatColor.YELLOW + "You are also now immune to Darkness.");
+		meta.setLore(lore);
+		meta.setUnbreakable(true);
+		item.setItemMeta(meta);
+		return item;
+	}
+
+	@Override
+	protected void onFire(EntityShootBowEvent event)
+	{
+		Arrow arrow = (Arrow) event.getProjectile();
+		arrow.setFireTicks(0);
+		arrow.setVelocity(arrow.getVelocity().multiply(1));
+		arrow.setKnockbackStrength(1);
+		arrow.setCritical(false);
+		arrow.spigot().setDamage(50); // Damage
+	}
+
+	@Override
+	protected boolean onHit(EntityDamageByEntityEvent event)
+	{
+
+	}
+}
