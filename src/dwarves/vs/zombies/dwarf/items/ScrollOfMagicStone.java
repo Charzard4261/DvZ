4130d2a4a61b05a9f44dc0e04500884ceb32c036
nessie2013 <thomasprentice01@hotmail.com>
2018-03-02 17:04:18 +0000

    changed item

--- src/dwarves/vs/zombies/dwarf/items/ScrollOfMagicStone.java
+++ src/dwarves/vs/zombies/dwarf/items/ScrollOfMagicStone.java
@@ -14,7 +14,7 @@ public class ScrollOfMagicStone extends DwarfItem{
 	@Override
 	public ItemStack getItem()
 	{
-		ItemStack item = new ItemStack(Material.INK_SACK, 1, (byte) 9);
+		ItemStack item = new ItemStack(Material.BRICK);
 		ItemMeta meta = item.getItemMeta();
 		
 		meta.setDisplayName(ChatColor.AQUA + "Scroll of Magic Stone.");
