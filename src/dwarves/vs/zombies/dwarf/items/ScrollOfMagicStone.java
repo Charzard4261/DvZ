581dd1244069186fea370840db711e247435410a
nessie2013 <thomasprentice01@hotmail.com>
2018-03-02 16:44:12 +0000

    added lore not changed item

---
+++ src/dwarves/vs/zombies/dwarf/items/ScrollOfMagicStone.java
@@ -0,0 +1,37 @@
+package dwarves.vs.zombies.dwarf.items;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import dwarves.vs.zombies.dwarf.superclasses.DwarfItem;
+
+public class ScrollOfMagicStone extends DwarfItem{
+
+	@Override
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (byte) 9);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.AQUA + "Scroll of Magic Stone.");
+		
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.YELLOW + 								"L-Click: To instantly make a wall of Magicstone!");
+		lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_PURPLE + 	"Trademarks expire?,");
+		lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_PURPLE + 	"No! My slab business.");
+    lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_PURPLE + 	"Noooooooooooooooooooooo");
+		lore.add(ChatColor.GOLD + 									"- Deadbones");
+		meta.setLore(lore);
+		
+		meta.setUnbreakable(true);
+		
+		item.setItemMeta(meta);
+		
+		return item;
+	}
+
+}
