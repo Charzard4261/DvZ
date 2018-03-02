9cbd56ea84b9560adbb936bf1636e0014e16172b
nessie2013 <thomasprentice01@hotmail.com>
2018-03-02 17:02:41 +0000

    changed item

--- src/dwarves/vs/zombies/dwarf/items/ArmorDiamond.java
+++ src/dwarves/vs/zombies/dwarf/items/ArmorDiamond.java
@@ -14,7 +14,7 @@ public class ArmorDiamond extends DwarfItem{
 	@Override
 	public ItemStack getItem()
 	{
-		ItemStack item = new ItemStack(Material.INK_SACK, 1, (byte) 9);
+		ItemStack item = new ItemStack(Material.DIAMOND);
 		ItemMeta meta = item.getItemMeta();
 		
 		meta.setDisplayName(ChatColor.AQUA + "Dwarven Armor");
