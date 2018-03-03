e590eb77fd37bbef91d0d10709d2a651c0788cac
nessie2013 <thomasprentice01@hotmail.com>
2018-03-03 18:56:47 +0000

    added lore
    didnt know the dye types so didnt change them

---
+++ src/dwarves/vs/zombies/dwarf/items/WigglyWrench.java
@@ -0,0 +1,31 @@
+ackage dwarves.vs.zombies.dwarf.items;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import dwarves.vs.zombies.dwarf.superclasses.DwarfItem;
+
+public class WigglyWrench extends DwarfItem{
+
+	@Override
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (byte) 9);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.AQUA + "Wiggly Wrench");
+		
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.YELLOW + 								"L-Click: Repair your armor to full");
+		meta.setLore(lore);
+		
+		item.setItemMeta(meta);
+		
+		return item;
+	}
+
+}
---
+++ src/dwarves/vs/zombies/dwarf/items/WizardMortar.java
@@ -0,0 +1,34 @@
+package dwarves.vs.zombies.dwarf.items;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import dwarves.vs.zombies.dwarf.superclasses.DwarfItem;
+
+public class WizzardMortar extends DwarfItem{
+
+	@Override
+	public ItemStack getItem()
+	{
+		ItemStack item = new ItemStack(Material.INK_SACK, 1, (byte) 9);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.AQUA + "Wizzard Mortar");
+		
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.YELLOW + 								"L-Click: Use on any walls to repair them into magic stone!");
+		lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_PURPLE + 	"use this carefully Jimmy. I don't have a lot of it and it's");
+		lore.add(ChatColor.ITALIC + "" + ChatColor.DARK_PURPLE + 	"strong enough to hold anything together");
+		lore.add(ChatColor.GOLD + 									"- Bruce Willakers");
+		meta.setLore(lore);
+		
+		item.setItemMeta(meta);
+		
+		return item;
+	}
+
+}
