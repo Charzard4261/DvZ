7c67b1d9146beba1d7c109ee1ad5343ac57b4752
nessie2013 <thomasprentice01@hotmail.com>
2018-03-07 13:35:25 +0000

    added proc on digging gravel with tm
    need to make sure it works

--- src/dwarves/vs/zombies/listeners/dwarves/BreakBlockListener.java
+++ src/dwarves/vs/zombies/listeners/dwarves/BreakBlockListener.java
@@ -11,7 +11,11 @@ import org.bukkit.inventory.ItemStack;
 import dwarves.vs.zombies.Core;
 import dwarves.vs.zombies.dwarf.items.ArmorDiamond;
 import dwarves.vs.zombies.dwarf.items.MightyPickaxe;
+import dwarves.vs.zombies.dwarf.equipment.swords.TombMaker;
 import dwarves.vs.zombies.enums.Stage;
+
+import dwarves.vs.zombies.dwarf.Dwarf;
+import dwarves.vs.zombies.dwarf.superclasses.DwarfSword;
 
 public class BreakBlockListener implements Listener {
 
@@ -66,8 +70,8 @@ public class BreakBlockListener implements Listener {
 			case GRAVEL:
 				event.getPlayer().playSound(event.getBlock().getLocation(), Sound.BLOCK_ANVIL_PLACE, 0.4f, 1f);
 				event.getPlayer().getInventory().addItem(new ItemStack(Material.COBBLESTONE, 2));
-				if (event.getPlayer().getInventory().getItemInMainHand().equals(new MightyPickaxe().getItem()))
-					getdwarf().proc(3);
+				if (event.getPlayer().getInventory().getItemInMainHand().equals(new TombMaker().getItem()))
+					getdwarf().proc(5);
 				break;
 			case GOLD_BLOCK:
 				event.getPlayer().getInventory().addItem(new ArmorDiamond().getItem());
